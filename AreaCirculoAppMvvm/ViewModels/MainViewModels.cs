fd44ad08aca9dbdfb6525065ccff6998e7977f0e
g3rar2 <113952421+g3rar2@users.noreply.github.com>
2025-03-15 12:58:16 -0600

    Adding Calculos

--- AreaCirculoAppMvvm/ViewModels/MainViewModels.cs
+++ AreaCirculoAppMvvm/ViewModels/MainViewModels.cs
@@ -1,6 +1,48 @@
-﻿namespace AreaCirculoAppMvvm.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public class MainViewModels
+namespace AreaCirculoAppMvvm.ViewModels;
+
+public partial class MainViewModels:ObservableObject
 {
+    
+    
+    private void Alerta(string Titulo, string Mensaje)
+    {
+        MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+    }
+
+    
+    
+    
+    [ObservableProperty]
+    private double radio;
+    
+    
+    [ObservableProperty]
+    private double area;
+    
+
+    [RelayCommand]
+    private void Calcular()
+    {
+        if (radio == 0)
+        {
+            Alerta("Error", "Ingrese el radio");
+        }
+        else
+        {
+            Area = Math.PI * Math.Pow(Radio, 2); 
+        }
+        
+    }
+    
+    [RelayCommand]
+    private void Limpiar()
+    {
+        Radio = 0;
+        Area = 0;
+    }
+
     
 }
--- AreaCirculoAppMvvm/Views/MainView.xaml.cs
+++ AreaCirculoAppMvvm/Views/MainView.xaml.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AreaCirculoAppMvvm.ViewModels;
 
 namespace AreaCirculoAppMvvm.Views;
 
 public partial class MainView : ContentPage
 {
+    
+    
+    private MainViewModels viewModel;
+    
     public MainView()
     {
         InitializeComponent();
+        viewModel = new MainViewModels();
+        BindingContext = viewModel;
     }
 }
